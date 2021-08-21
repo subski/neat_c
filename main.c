423767aeaf32b35b133f710cd99efd40cf8169a4
subski <58097205+subski@users.noreply.github.com>
2021-08-21 06:03:55 +0200

    updated visual studio specific

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
 
-int main()
+#ifdef _WIN64 || _WIN32
+#include <Windows.h>
+#endif
+
+
+#include "agent.h"
+#include "neuron.h"
+#include "env_settings.h"
+#include "clist.h"
+
+int main(void)
 {
-    printf("Hello world");
-    return 0;
+    
+    return EXIT_SUCCESS;
 }
