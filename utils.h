875e848fa592d4105e230d3947d462f2a9af2843
subski <58097205+subski@users.noreply.github.com>
2021-08-23 21:18:23 +0200

    Removed typedefs for integers as we will now use the built-in keywords (e.g. uint16_t  become unsigned short).

--- utils.h
+++ utils.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -9,16 +8,4 @@
 		return ret;				\
 	}
 
-#ifndef _STDINT
-
-typedef signed char        int8_t;
-typedef short              int16_t;
-typedef int                int32_t;
-typedef long long          int64_t;
-typedef unsigned char      uint8_t;
-typedef unsigned short     uint16_t;
-typedef unsigned int       uint32_t;
-typedef unsigned long long uint64_t;
-
-#endif // !_STDINT
 #endif // !UTILS_H
