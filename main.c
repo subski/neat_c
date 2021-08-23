b84f709e4b420a5ee60ad6a94d68f9c58debc6a3
subski <58097205+subski@users.noreply.github.com>
2021-08-23 21:26:55 +0200

    Testing agent memory allocation.

--- main.c
+++ main.c
@@ -15,13 +15,51 @@
 #include "pool.h"
 #include "neuron.h"
 #include "agent.h"
-
-static pool* P_AGENT = NULL;
-static pool* P_NEURON = NULL;
-static pool* P_LINK = NULL;
+#include "neurolution.h"
 
 int main(void)
 {
+	clist* agents = NULL;
+	for (int i = 0; i < 1000; i++)
+	{
+		insert(&agents, createBasicAgent());
+	}
+
+	clist* tmp;
+	for (int i = 0; i < 500; i++)
+	{
+		tmp = agents->next;
+		agents->next = tmp->next;
+		free_agent(&tmp->data);
+		free(tmp);
+	}
+
+	printf("plink %d\n", len(P_LINK));
+	printf("pneuron %d\n", len(P_NEURON));
+	printf("pagent %d\n", len(P_AGENT));
+
+	for (int i = 0; i < 600; i++)
+	{
+		insert(&agents, createBasicAgent());
+	}
+
+	printf("plink %d\n", len(P_LINK));
+	printf("pneuron %d\n", len(P_NEURON));
+	printf("pagent %d\n", len(P_AGENT));
+
+	clist* iter = agents;
+	do
+	{
+		free_agent((Agent**)&iter->data);
+		next(iter);
+	} while (iter != agents);
+
+	clear(&agents);
+
+	clean(&P_LINK);
+	clean(&P_NEURON);
+	clean(&P_AGENT);
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
