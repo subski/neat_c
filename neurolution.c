aa104e9812d65277a1b214528a47fc7fb0cebbce
subski <58097205+subski@users.noreply.github.com>
2021-08-23 21:16:47 +0200

    Added neurolution code files to hold the code that will supervise the evolution algorithm. For now it just hold the agent/neuron/link memory pool allocators.

---
+++ neurolution.c
@@ -0,0 +1,7 @@
+#include "neurolution.h"
+
+#include "pool.h"
+
+pool* P_AGENT = NULL;
+pool* P_NEURON = NULL;
+pool* P_LINK = NULL;
---
+++ neurolution.h
@@ -0,0 +1,11 @@
+#ifndef NEUROLUTION_H
+#define NEUROLUTION_H
+
+typedef struct clist clist;
+typedef clist pool;
+
+extern pool* P_AGENT;
+extern pool* P_NEURON;
+extern pool* P_LINK;
+
+#endif // !NEUROLUTION_H
