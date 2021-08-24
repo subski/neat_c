512dcc9411600fc8bd0593ddf76d0ac10a1575eb
subski <58097205+subski@users.noreply.github.com>
2021-08-24 02:31:45 +0200

    Performance test between using only free/malloc and with the usage of pools of memory. It seems to be no notable difference when the number of space being allocated is constantly increasing. But when we have a pretty constant number of object being allocated and freed, we notice a significant amount of time being saved when using pools. (refer to the code in this commit for the test.) In conclusion, no time is wasted when using pools and in some case it can make the program run faster.

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <errno.h>
 
+#include <time.h>
+
 #include <Windows.h>
 
 #include "utils.h"
@@ -17,35 +19,106 @@
 #include "agent.h"
 #include "neurolution.h"
 
+
+Agent* cBA(void)
+{
+
+	Agent* new_agent = malloc(sizeof(Agent));
+
+	new_agent->fitness = 0.0;
+	new_agent->neurons = NULL;
+	new_agent->links = NULL;
+
+	// create input neurons
+	for (int i = 0; i < 3; i++)
+	{
+		Neuron* input_neuron = malloc(sizeof(Neuron));
+
+		input_neuron->id = i + 1;
+		input_neuron->enabled = true;
+		input_neuron->activated = false;
+		input_neuron->type = INPUT_TYPE;
+		input_neuron->activationFunc = NULL;
+		input_neuron->value = 0.0;
+		input_neuron->bias = 0.0;
+		input_neuron->links = NULL;
+
+		new_agent->inputNeurons[i] = input_neuron;
+		insert(&new_agent->neurons, input_neuron);
+	}
+
+	// create output neurons
+	for (int i = 0; i < 2; i++)
+	{
+		Neuron* output_neuron = malloc(sizeof(Neuron));
+
+		output_neuron->id = 3 + i + 1;
+		output_neuron->enabled = true;
+		output_neuron->activated = false;
+		output_neuron->type = OUTPUT_TYPE;
+		output_neuron->activationFunc = NULL;
+		output_neuron->value = 0.0;
+		output_neuron->bias = 0.0;
+		output_neuron->links = NULL;
+
+		new_agent->outputNeurons[i] = output_neuron;
+		insert(&new_agent->neurons, output_neuron);
+	}
+
+	// create links
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			Link* new_link = malloc(sizeof(Link));
+
+			new_link->source = new_agent->inputNeurons[j];
+			new_link->weight = 1.0;
+			new_link->activated = true;
+
+			insert(&new_agent->outputNeurons[i]->links, new_link);
+			insert(&new_agent->links, new_link);
+		}
+	}
+
+	return new_agent;
+}
+
 int main(void)
 {
+
 	clist* agents = NULL;
-	for (int i = 0; i < 1000; i++)
-	{
-		insert(&agents, createBasicAgent());
-	}
-
 	clist* tmp;
-	for (int i = 0; i < 500; i++)
+	clock_t start_time;
+
+
+
+
+	_sleep(3000);
+
+	start_time = clock();
+
+	for (int k = 0; k < 100; k++)
 	{
-		tmp = agents->next;
-		agents->next = tmp->next;
-		free_agent(&tmp->data);
-		free(tmp);
+
+
+		for (int i = 0; i < 20000; i++)
+		{
+			insert(&agents, createBasicAgent());
+		}
+
+		for (int i = 0; i < 19990; i++)
+		{
+			tmp = agents->next;
+			agents->next = tmp->next;
+			free_agent(&tmp->data);
+			free(tmp);
+		}
+
+
 	}
 
-	printf("plink %d\n", len(P_LINK));
-	printf("pneuron %d\n", len(P_NEURON));
-	printf("pagent %d\n", len(P_AGENT));
-
-	for (int i = 0; i < 600; i++)
-	{
-		insert(&agents, createBasicAgent());
-	}
-
-	printf("plink %d\n", len(P_LINK));
-	printf("pneuron %d\n", len(P_NEURON));
-	printf("pagent %d\n", len(P_AGENT));
+	printf("%d\n", clock() - start_time);
 
 	clist* iter = agents;
 	do
@@ -59,6 +132,47 @@ int main(void)
 	clean(&P_LINK);
 	clean(&P_NEURON);
 	clean(&P_AGENT);
+
+
+	_sleep(3000);
+
+	
+	start_time = clock();
+
+	for (int k = 0; k < 100; k++)
+	{
+
+		for (int i = 0; i < 20000; i++)
+		{
+			insert(&agents, cBA());
+		}
+
+		for (int i = 0; i < 19990; i++)
+		{
+			tmp = agents->next;
+			agents->next = tmp->next;
+
+			Agent* agent = tmp->data;
+			clean(&agent->links);
+			CLIST_ITER(agent->neurons, neuron_node,
+				clear(&((Neuron*)neuron_node->data)->links);
+			)
+			clean(&agent->neurons);
+			free(agent);
+
+
+			free(tmp);
+		}
+
+
+	}
+	printf("%d\n", clock() - start_time);
+
+	CLIST_ITER(agents, agent_node,
+		clean(&((Agent*)agents->data)->links);
+		clean(&((Agent*)agents->data)->neurons);
+	)
+	clean(&agents);
 
 	system("pause");
 	return EXIT_SUCCESS;
